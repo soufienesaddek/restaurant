00c6bace3b4c38aa9bf711dbd8bf94dbbef76d3e
Soufiene AIT ALI <fatfataassr@gmail.com>
2023-05-19 10:12:44 +0100

    correction

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <SubHeading title="Recherchez de nouvelles saveurs" />
-      <h1 className="app__header-h1">La clé pour d'un bon repas</h1>
+      <h1 className="app__header-h1">La clé pour un bon repas</h1>
       <p className="p__opensans" style={{margin: '2rem 0'}}>Savourez l'excellence culinaire dans un cadre enchanteur&nbsp;: un voyage gastronomique inoubliable !</p>
       <button type="button" className="custom__button">Découvrez le menu</button>
     </div>
